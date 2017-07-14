c2533b1cab2e2cf3e6c0de8b9e4bfba143f58da3
Stephanie Nemeth <traumverloren@gmail.com>
2017-07-14 13:32:14 +0200

    circles in svg

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
 import * as d3 from 'd3';
 import './styles.css';
 
-const dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19,
-                14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
-                24, 18, 25, 9, 3 ];
+let dataset1 = [];                        //Initialize empty array
+for (let i = 0; i < 25; i++) {           //Loop 25 times
+    let newNumber = Math.random() * 30;  //New random number (0-30)
+    dataset1.push(newNumber);             //Add new number to array
+}
 
-    d3.select("body").selectAll("div")
-        .data(dataset)
-        .enter()
-        .append("div")
-        .attr("class", "bar")
-        .style("height", function(d) {
-          var barHeight = d * 5;  //Scale up by factor of 5
-          return barHeight + "px";
-        });
+// bar graph
+let graph = d3.select("body").selectAll("div")
+    .data(dataset1)
+    .enter()
+    .append("div")
+    .attr("class", "bar")
+    .style("height", function(d) {
+      let barHeight = d * 5;  //Scale up by factor of 5
+      return barHeight + "px";
+    });
+
+let br = d3.select("body").append('br');
+
+// svg generation
+const dataset2 = [ 5, 10, 15, 20, 25 ];
+let svg = d3.select("body").append("svg");
+
+let width = 500;
+let height = 100;
+
+svg.style("width", width);
+svg.style("height", height);
+
+let circles = svg.selectAll("circle")
+    .data(dataset2)
+    .enter()
+    .append("circle");
+
+circles.attr("cx", function(d, i) {
+            return (i * 50) + 50;
+        })
+       .attr("cy", height/2)
+       .attr("r", function(d) {
+            return d;
+       })
+       .attr("fill", "teal")
+      .attr("stroke", "darkturquoise")
+      .attr("stroke-width", function(d) {
+          return d/2;
+      });
