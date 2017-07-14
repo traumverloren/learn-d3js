e4fb1c9ebc866ff3066ce9a65cca72be2ec970cc
Stephanie Nemeth <traumverloren@gmail.com>
2017-07-14 14:16:39 +0200

    add readme, bar graph now in svg with labels

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning D3.js
+
+I checked out this [tutorial](http://alignedleft.com/tutorials/d3) to get my feet wet with data visualizations with D3.js.
--- src/index.js
+++ src/index.js
@@ -1,36 +1,65 @@
 import * as d3 from 'd3';
 import './styles.css';
 
-let dataset1 = [];                        //Initialize empty array
-for (let i = 0; i < 25; i++) {           //Loop 25 times
-    let newNumber = Math.random() * 30;  //New random number (0-30)
-    dataset1.push(newNumber);             //Add new number to array
-}
+let dataset1 = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+                11, 12, 15, 20, 18, 17, 16, 18, 23, 25];                        //Initialize empty array
+
+let svg1 = d3.select("body")
+           .append("svg")
+           .attr("width", width)
+           .attr("height", height);
+
+const width = 500;
+const height = 100;
+const barPadding = 1;
 
 // bar graph
-let graph = d3.select("body").selectAll("div")
+const graph = svg1.selectAll("rect")
     .data(dataset1)
     .enter()
-    .append("div")
-    .attr("class", "bar")
-    .style("height", function(d) {
-      let barHeight = d * 5;  //Scale up by factor of 5
-      return barHeight + "px";
-    });
+    .append("rect")
+    .attr("x", function(d, i) {
+      return i * (width / dataset1.length);
+    })
+    .attr("y", function(d) {
+      return height - (d*4);
+    })
+    .attr("width", width / dataset1.length - barPadding)
+    .attr("height", function(d) {
+      return d*4;
+    })
+    .attr("fill", function(d) {
+    return `rgb(0, ${d * 10}, ${d * 10})`;
+});
 
-let br = d3.select("body").append('br');
+const graphLabels = svg1.selectAll("text")
+    .data(dataset1)
+    .enter()
+    .append("text")
+    .text(function(d) {
+      return d;
+    })
+    .attr("text-anchor", "middle")
+    .attr("x", function(d, i) {
+        return i * (width / dataset1.length) + (width / dataset1.length - barPadding) / 2;
+   })
+   .attr("y", function(d) {
+        return height - (d * 4) + 14;
+   })
+   .attr("font-family", "sans-serif")
+   .attr("font-size", "11px")
+   .attr("fill", "white");
+
+const br = d3.select("body").append('br');
 
 // svg generation
 const dataset2 = [ 5, 10, 15, 20, 25 ];
-let svg = d3.select("body").append("svg");
+const svg2 = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
 
-let width = 500;
-let height = 100;
-
-svg.style("width", width);
-svg.style("height", height);
-
-let circles = svg.selectAll("circle")
+const circles = svg2.selectAll("circle")
     .data(dataset2)
     .enter()
     .append("circle");
